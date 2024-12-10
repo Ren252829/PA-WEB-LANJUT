25dc6b642229c082fe60ca7d5faa08d6b9d3c349
Dirga <apriliansyahdirga@gmail.com>
2024-12-11 06:02:37 +0800

    fix front-end (merge)

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -18,24 +18,4 @@ export const getProjectById = async (projectId) => {
     console.error(`Error fetching project with ID ${projectId}:`, error);
     throw error;
   }
-};import axios from '../axios'; // Mengimpor Axios yang sudah dikonfigurasi
-
-export const getAllProjects = async () => {
-  try {
-    const response = await axios.get('/projects');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching all projects:', error);
-    throw error;
-  }
-};
-
-export const getProjectById = async (projectId) => {
-  try {
-    const response = await axios.get(`/projects/${projectId}`);
-    return response.data;
-  } catch (error) {
-    console.error(`Error fetching project with ID ${projectId}:`, error);
-    throw error;
-  }
 };
